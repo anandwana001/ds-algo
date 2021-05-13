f9e6a6467fbb21971bf6b1c5415f155d768b6723
anandwana001 <akshaynandwana001@gmail.com>
2021-05-13 10:39:40 +0530

    practice

---
+++ leetcode/src/codechef/RajuAndHisTrip.java
@@ -0,0 +1,47 @@
+package codechef;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class RajuAndHisTrip {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+
+        InputReader in = new InputReader(inputStream);
+        int N = in.nextInt();
+
+        if(N % 5 == 0 || N % 6 == 0)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
